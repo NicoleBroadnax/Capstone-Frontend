12e39bcb665613c25060797861a506896fefaeaf
_NBX_ <104787483+NicoleBroadnax@users.noreply.github.com>
2022-09-01 18:19:04 -0400

    Update

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import APIUrl from "../../APIUrl";
+import Comment from "../Comment";
 
 //import useParams from react-router-dom
 //import useState from react
 const Service = () => {
   const [service, setService] = useState(); //create state to store serivce
+  const [content, setContent] = useState();
   const params = useParams(); //add const params = useParams();
   useEffect(() => {
     //add useEffect
@@ -18,6 +21,11 @@ const Service = () => {
     };
     getService(); //call getService()
   }, []);
+
+  const createNewComment = () => {
+    
+  };
+
   if (!service) {
     //if(!store){
     return <h1>Loading</h1>; //   return <h1>Loading</h1>
@@ -25,9 +33,47 @@ const Service = () => {
 
   return (
     <div>
-      <h1>{service.name}</h1>
+      <h1>service</h1>
+      {service.comments.map((comment) => {
+        return (
+          <div key={comment.rating}>
+            {comment.content}
+            <p>{comment.serviceid}</p>
+          </div>
+        );
+      })}
+      <form onSubmit={createNewComment}>
+        <label>Content</label>
+        <input
+          type="text"
+          className="form-control"
+          value={content}
+          onChange={(evt) => {
+            setContent(evt.target.value);
+          }}
+        />
+        <br />
+        <label>Post Content:</label>
+        <textarea
+          className="form-control"
+          value={content}
+          onChange={(evt) => {
+            setContent(evt.target.value);
+          }}
+        ></textarea>
+        <br />
+        <button type="submit" className="btn btn-primary">
+          Post
+        </button>
+      </form>
     </div>
   );
 };
 
 export default Service;
+
+{
+  /* <div>
+<h1>{service.name}</h1>
+</div> */
+}
